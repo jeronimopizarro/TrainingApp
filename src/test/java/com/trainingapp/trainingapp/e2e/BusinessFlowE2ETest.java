0f95e49729cc8d39aa49a1da666fa8e10180d8a7
Jero <jeronimo1080@hotmail.com>
2026-04-10 22:47:19 -0300

    fix: estabilizar CI moviendo configuración de tests y eliminando dependencias de Docker

--- src/test/java/com/trainingapp/trainingapp/e2e/BusinessFlowE2ETest.java
+++ src/test/java/com/trainingapp/trainingapp/e2e/BusinessFlowE2ETest.java
@@ -18,6 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -38,6 +39,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @Transactional
 class BusinessFlowE2ETest {
 
